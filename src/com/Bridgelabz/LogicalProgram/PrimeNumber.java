6ade9f5220254cb04f86dcee024e15664f3fa502
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-07-31 00:32:42 +0530

    Logical Programs Added

--- src/com/Bridgelabz/LogicalProgram/PrimeNumber.java
+++ src/com/Bridgelabz/LogicalProgram/PrimeNumber.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
+        int num1 , count = 0;
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Welcome to the Prime Number program.");
         System.out.println("Please enter any number to check prime number.");
-        int num1 = keyboard.nextInt();
-        for (int i = 2 ; i < num1; i++) {
-            if ((num1 % i) == 0) {
-                System.out.println(num1 + " is prime number");
+        num1 = keyboard.nextInt();
+        for (int i = 1; i <= num1; i++) {
+            if (num1 % i == 0) {
+                count++;
             }
+        } if (count == 2) {
+                System.out.println(num1 + " is Prime Number.");
+            } else
+                System.out.println(num1+" is not Prime Number.");
         }
     }
-}
